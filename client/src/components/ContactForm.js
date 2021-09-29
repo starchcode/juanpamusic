e2236a73ad6c3287a161c181d795e08f579b881b
Starch <mrdave67@gmail.com>
2021-09-29 20:13:21 +0100

    contact form init

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,18 +1,106 @@
-import React from 'react';
+import React from "react";
+import { Form, Field } from "react-final-form";
 
-import './css/contactForm.css';
+import "./css/contactForm.css";
 
 class ContactForm extends React.Component {
-
-    render(){
-        return(
-            <div id="contactForm">
-                Contact
-            </div>
-        )
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="error">
+          <div className="">{error}</div>
+        </div>
+      );
     }
+  };
+
+  renderInput = ({ input, label, meta }) => {
+    const patterns = {
+      Email: "[a-zA-Z0-9._%+-]+@[[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$",
+      Phone: "([+])?[0-9]+",
+    };
+    const className = `${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        {input.name == "Enquiry" ? (
+          <textarea
+            {...input}
+            id=""
+            placeholder="Please write your message..."
+            maxLength="350"
+          ></textarea>
+        ) : (
+          <input
+            {...input}
+            autoComplete="off"
+            pattern={patterns[input.name] ? patterns[input.name] : null}
+            maxLength={input.name == "Enquiry" ? 250 : null}
+          />
+        )}
+
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit(formValues) {
+    console.log("submit", formValues);
+  }
+
+  render() {
+    return (
+      <div id="contactComponent">
+        <div id="contactForm">
+          <Form
+            initialValues={{
+              Name: "Dave",
+              Email: "a@a.com",
+              Phone: 0,
+              Enquiry: "hi...",
+            }}
+            validate={(formValues) => {
+              const errors = {};
+
+              if (!formValues.Name) {
+                errors.Name = "Please enter your name";
+              }
+
+              if (!formValues.Email) {
+                errors.Email = "Please enter your Email";
+              }
+              if (!formValues.Enquiry) {
+                errors.Enquiry = "Please enter your enquiry";
+              }
+              return errors;
+            }}
+            onSubmit={this.onSubmit}
+            render={({ handleSubmit }) => (
+              <form onSubmit={handleSubmit} className="ui form error">
+                <Field name="Name" component={this.renderInput} label="Name" />
+                <Field
+                  name="Email"
+                  component={this.renderInput}
+                  label="Email"
+                />
+                <Field
+                  name="Phone"
+                  component={this.renderInput}
+                  label="Phone"
+                />
+                <Field
+                  name="Enquiry"
+                  component={this.renderInput}
+                  label="Enquiry"
+                />
+                <button className="">Submit</button>
+              </form>
+            )}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ContactForm;
-
-
